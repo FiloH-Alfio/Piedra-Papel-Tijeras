1acf5c90f4a1517b721e3393e6deff60e7747f0a
FiloH <taseguin@gmail.com>
2025-02-18 12:09:48 -0300

    Se coloco el nombre del usuario en la segunda solicitud

--- Piedra-Papel-tijeras.py
+++ Piedra-Papel-tijeras.py
@@ -1,8 +1,11 @@
 nombre1 = input ("Como se llamará el Jugador 1?: ")
 nombre2 = input ("Como se llamará el Jugador 2?: ")
 
-Jugador1 = input("Bienvenido", nombre1, "Qué elegis? Piedra, Papel o Tijeras?: ")
-Jugador2 = input("Bienvenido", nombre2, "Qué elegis? Piedra, Papel o Tijeras?: ")
+
+
+Jugador1 = input("Bienvenido " + nombre1 + "! qué elegis? Piedra, Papel o Tijeras?: ")
+Jugador2 = input("Bienvenido " + nombre2 + "! qué elegis? Piedra, Papel o Tijeras?: ")
+
 
 condicion1 = Jugador1 == "piedra" and Jugador2 == "tijeras"
 condicion2 = Jugador1 == "papel" and Jugador2 == "piedra"
@@ -11,7 +14,7 @@ condicion3 = Jugador1 == "tijeras" and Jugador2 == "papel"
 if Jugador1 == Jugador2:
     print("Ha sido un empate!")
 elif condicion1 or condicion2 or condicion3:
-    print("Has ganado:", nombre1)
+    print("Has ganado", nombre1)
 else:
-    print("Has ganador:", nombre2)
+    print("Has ganado", nombre2)
 
